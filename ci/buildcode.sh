747876dcd12e7451e8e22e1f3941edcef600a720
jimmy shah <jimmyshah83@gmail.com>
2020-02-19 17:47:22 -0500

    Resolving deploy issues

--- ci/buildcode.sh
+++ ci/buildcode.sh
@@ -2,6 +2,4 @@
 
 cd git-repo
 mvn clean package
-mkdir ../artifacts/
-chmod 755 ../artifacts/
-cp -p target/pws-spring-boot-demo*.jar ../artifacts/
+cp -p target/pws-spring-boot-demo*.jar ../build-output/
