41cf3894a4086808ae5d6feb4ea446f65e0e1938
Jimmy Shah <jimmyshah83@gmail.com>
2020-06-17 17:19:31 -0400

    Data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+delete from Employee;
+insert into Employee (Id, name, city, department) values (1, 'John', 'Toronto', 'ABC');
+insert into Employee (Id, name, city, department) values (1, 'Terry', 'Windsor', 'DDD');
